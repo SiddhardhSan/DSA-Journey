5e169dc94eec7f5ce4c758fc0bb634052a71d1cc
SiddhardhSan <sanivadasiddhardh01@gmail.com>
2025-08-14 21:53:37 +0530

    Insertion Sort

---
+++ sorting/insertionSort.cpp
@@ -0,0 +1,28 @@
+//insertion sprt
+#include <iostream>
+using namespace std;
+void insertionSort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        int j=i;
+        while(j>0&&arr[j-1]>arr[j]){
+            int temp=arr[j-1];
+            arr[j-1]=arr[j];
+            arr[j]=temp;
+            j--;
+
+        }
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    insertionSort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
